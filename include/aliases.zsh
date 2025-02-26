5806294dcbc024ae85df7a440d59f4e8a1b2b746
Caleb Xu <calebcenter@live.com>
2025-02-26 12:11:12 -0500

    Alias code to code/codium depending on availability

--- include/aliases.zsh
+++ include/aliases.zsh
@@ -2,7 +2,12 @@ alias cask="brew cask"
 alias androidscreen="(adb shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g') | imgcat"
 alias ls="$(brew --prefix)/opt/coreutils/libexec/gnubin/ls --color=auto -CFGhp"
 alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
-alias code="/Applications/VSCodium.app/Contents/Resources/app/bin/codium"
 alias bbfpr="brew bump-formula-pr --no-browse"
 alias bbcpr="brew bump-cask-pr --no-browse"
 alias line="lima nerdctl"
+
+if [ -e "/Applications/VSCodium.app/Contents/Resources/app/bin/codium" ]; then
+    alias code="/Applications/VSCodium.app/Contents/Resources/app/bin/codium"
+elif [ -e "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" ]; then
+    alias code="/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
+fi
