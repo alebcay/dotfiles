c577dfc9d0b601d709b8a45c4fd244cbdddef3a0
Caleb Xu <calebcenter@live.com>
2026-07-16 11:04:57 -0400

    starship: make clean_win_title and set_win_title portable

--- include/starship.zsh
+++ include/starship.zsh
@@ -1,5 +1,11 @@
 function clean_win_title() {
-    echo -ne "\e]0;\a"
+    echo -ne "\e]0;\a\e\\"
 }
 
-precmd_functions+=("clean_win_title")
+function set_win_title() {
+    local title
+    title=$(print -Pn "%n@%m: %~")
+    echo -ne "\e]0;${title}\e\\"
+}
+
+precmd_functions+=("clean_win_title" "set_win_title")
