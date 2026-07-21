763b8acc4cab0e467af3117722ae2604ea556ecc
Caleb Xu <calebcenter@live.com>
2026-07-21 10:16:13 -0400

    Simplify aliases

--- include/aliases.zsh
+++ include/aliases.zsh
@@ -1,13 +1,27 @@
+function alias_cascade() {
+    local a="$1"
+    shift
+
+    while [[ $# -ne 0 ]]; do
+        local p="$1"
+        if [ -e "$p" ]; then
+            eval "alias ${a}=\"${p}\""
+            return
+        fi
+        shift
+    done
+}
+
 alias cask="brew cask"
 alias androidscreen="(adb shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g') | imgcat"
-alias ls="$(brew --prefix)/opt/coreutils/libexec/gnubin/ls --color=auto -CFGhp"
+alias_cascade ls "$(brew --prefix)/opt/coreutils/libexec/gnubin/ls"
 alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
 alias bbfpr="brew bump-formula-pr --no-browse"
 alias bbcpr="brew bump-cask-pr --no-browse"
 alias line="lima nerdctl"
 
-if [ -e "/Applications/VSCodium.app/Contents/Resources/app/bin/codium" ]; then
-    alias code="/Applications/VSCodium.app/Contents/Resources/app/bin/codium"
-elif [ -e "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" ]; then
-    alias code="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
-fi
+alias_cascade code "/Applications/VSCodium.app/Contents/Resources/app/bin/codium" \
+                   "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" \
+                   "${HOME}/AppImages/vscodium.appimage"
+
+unset -f alias_cascade
